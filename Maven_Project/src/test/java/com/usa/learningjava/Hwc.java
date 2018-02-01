d4b9ddbfdabb66c3e54eb1f8873b77aaf2b9dc1e
Sayad <Sayad@LAPTOP-NOO1O6PJ.fios-router.home>
2018-01-31 22:08:02 -0500

    Version 5.5

---
+++ Maven_Project/src/test/java/com/usa/learningjava/Hwc.java
@@ -0,0 +1,14 @@
+package com.usa.learningjava;
+
+public class Hwc extends Hwp {
+	
+	public static void main(String[] args) {
+		
+		Hwc obj = new Hwc();
+		
+		System.out.println(obj.name + " salary is " + obj.salary);
+		System.out.println(obj.name1 + " salary is " + obj.salary1);
+		
+	}
+
+}
---
+++ Maven_Project/src/test/java/com/usa/learningjava/Hwp.java
@@ -0,0 +1,13 @@
+package com.usa.learningjava;
+
+public class Hwp {
+
+int salary = 1000;
+
+String name = "King";
+
+int salary1 = 2000;
+
+String name1 = "John";
+	
+}
